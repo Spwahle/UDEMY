846c0d1c5257d4dd800328cc5204670df993074e
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-17 13:39:50 -0700

    Guessing game

---
+++ js_exercise/app.js
@@ -0,0 +1,59 @@
+// 'use strict'
+//
+// var finish = '';
+//
+// // var Questions = function(){
+//   // var firstName = prompt('Hello, what is your first name?');
+//   // var lastName = prompt('What is your last name?');
+//   var userAge = prompt('What is your age?');
+// // };
+// // //
+// // // Questions();
+// // var dayAlive = (userAge*365);
+// //
+// //
+// // document.write('You\'ve been alive ' + dayAlive + 'days!')
+//
+// // document.write('Hello, ' + firstName + ' ' + lastName + '! You are ' + userAge + ' years old')
+// // function isOdd(num) { return num % 2;}
+//
+//
+//
+// if (userAge < 18) {
+//   document.write('You\'re too young!')
+// }
+// else if (userAge < 21) {
+//   document.write('You can come in but you can\'t drink.')
+// }
+// else if (userAge == 21) {
+//   document.write('Happy Birthday')
+// }
+// else {
+//   document.write('Come in and enjoy yourself!')
+// }
+//
+// function isOdd(userAge) { return userAge % 2;
+//   if (userAge == 1)
+//   document.write('Your age is odd')
+//   else {
+//     document.write('Your age is even')
+//   }}
+// console.log("1 is " + isOdd(userAge));
+
+var secretNum = 13;
+
+var guess = prompt('Guess a number?');
+document.write(guess);
+
+if (guess == secretNum) {
+  document.write('You are right!');
+}
+else if (guess > secretNum){
+  document.write('Too high');
+}
+else if (guess < secretNum){
+  document.write('Too low');
+}
+else {
+  document.write('Not a number')
+}
