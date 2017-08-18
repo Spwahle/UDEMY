07194a187e0f56d23e6bbef41e4c04afc6e3f376
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-18 14:28:06 -0700

    CSS Styling

--- ToDo-List/assets/js/todo.js
+++ ToDo-List/assets/js/todo.js
@@ -1,8 +1,23 @@
 'use strict'
 
-$('li').click(function() {
-  $(this).css({
-    color: 'gray',
-    textDecoration: 'line-through'
-  });
+//toggling for done and undone items
+$('li').on('click', 'li', function() {
+  $(this).toggleClass('completed')
 });
+
+//using the span to help remove items
+$('ul').on('click', 'span', function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+//adding items to the list
+$('input[type="text"').keypress(function(event){
+  if(event.which === 13) {
+    let $newToDo = $(this).val();
+    $(this).val('');
+    $('ul').append('<li><span>X</span> ' + $newToDo + '</li>')
+  }
+})
