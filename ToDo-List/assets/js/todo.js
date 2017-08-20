c48fc8d968c59a74a466ad855c21a9dfeaf419ba
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-20 10:15:38 -0700

    Finished TODO

--- ToDo-List/assets/js/todo.js
+++ ToDo-List/assets/js/todo.js
@@ -1,23 +1,26 @@
-'use strict'
-
-//toggling for done and undone items
-$('li').on('click', 'li', function() {
-  $(this).toggleClass('completed')
+// Check Off Specific Todos By Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
 });
 
-//using the span to help remove items
-$('ul').on('click', 'span', function(event){
-  $(this).parent().fadeOut(500, function(){
-    $(this).remove();
-  });
-  event.stopPropagation();
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
 
-//adding items to the list
-$('input[type="text"').keypress(function(event){
-  if(event.which === 13) {
-    let $newToDo = $(this).val();
-    $(this).val('');
-    $('ul').append('<li><span><i class="fa fa-trash"></i></span> ' + $newToDo + '</li>')
-  }
-})
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle()
+});
