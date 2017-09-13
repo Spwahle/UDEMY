933ce47db47d15668ca76b5fb941b772d0444c18
Shaun Wahle <shaun.wahle@gmail.com>
2017-09-13 10:30:44 -0700

    Stacks and Queues

---
+++ stack.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Node = require('./node');
+
+module.exports = class {
+  constructor(){
+    this.top =null
+  }
+
+  push(val) {
+    let node = new Node(val);
+
+    if(!this.top) {
+      this.top = node;
+      return this.top
+    }
+
+    node.next = this.top
+    this.top = node
+    return this.top
+  }
+
+  pop()
+}
