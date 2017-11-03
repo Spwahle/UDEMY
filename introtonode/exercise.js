754e958706e5e2d22758f415cccb963397d6763c
Shaun Wahle <shaun.wahle@gmail.com>
2017-11-03 12:02:02 -0700

    adds getting warmed up

---
+++ introtonode/exercise.js
@@ -0,0 +1,10 @@
+function exercise(str, int) {
+
+
+for(var i = 0; i < int; i++) {
+  console.log(str)
+  }
+}
+
+exercise('echo!!!', 10)
+exercise('tater tots', 3)
---
+++ introtonode/objectives.md
@@ -0,0 +1,4 @@
+*What is node?
+Node is a server side language written in javascript
+
+*Why are we learning it?
