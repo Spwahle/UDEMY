9bb77ac978439e197d5d9fee4cc23f356068e668
Shaun Wahle <shaun.wahle@gmail.com>
2017-11-05 12:55:43 -0800

    finished todays assignment

--- introtonode/introinexpress/app.js
+++ introtonode/introinexpress/app.js
@@ -3,19 +3,40 @@ var app = express();
 
 
 app.get('/', function(req, res){
-  res.send('hi there!');
+  res.send('Hi there, welcome to my assignment!');
 });
 
-app.get('/bye', function(req, res){
-  res.send('See ya punk!');
+app.get('/speak/:animal', function(req, res){
+  var sounds = {
+    pig: "Oink",
+    cow: "Moo",
+    cat: "Meow",
+    dog: "Woof",
+    fox: "GIGJGTNHKJHNDKJBNDGKFB"
+
+  }
+  var animal = req.params.animal.toLowerCase();
+  var sound = sounds[animal];
+  res.send('The ' + animal + ' says ' + sound);
 });
 
-app.get('/dog', function(req, res){
-  res.send('MEOW!');
+app.get('/repeat/:message/:times', function(req, res){
+  var message = req.params.message;
+  var times = Number(req.params.times);
+  var result = '';
+
+  for (var i = 0; i < times; i++) {
+    result += message + ' ';
+  }
+    res.send(result);
+})
+
+app.get('/r/:subredditName/comments/:id/:title/', function(req, res){
+  res.send('Welcome to the comments page');
 });
 
 app.get('*', function(req, res){
-  res.send('you are a star');
+  res.send('Sorry, page not found.....what are you doing with your life?');
 });
 
 
