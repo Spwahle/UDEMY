10034081902490d41398e8809a070ebba1bfca0c
Shaun Wahle <shaun.wahle@gmail.com>
2017-12-07 10:51:55 -0800

    adds search

--- moviesearch/app.js
+++ moviesearch/app.js
@@ -1,18 +1,22 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
+app.get('/', function(req, res) {
+    res.render('search');
+});
 
 app.get('/results', function(req, res) {
-    request("http://omdbapi.com/?s=ohio&apikey=thewdb", function(error, response, body) {
+    request('http://omdbapi.com/?s=ohio&apikey=thewdb', function(error, response, body) {
+        console.log(req.query.search)
         if (!error && response.statusCode == 200) {
             var data = JSON.parse(body);
-            res.render("results", { data: data });
+            res.render('results', { data: data });
         }
     });
 });
 
 app.listen(3000, function() {
-    console.log("movie search starter server started");
+    console.log('movie search starter server started');
 });
